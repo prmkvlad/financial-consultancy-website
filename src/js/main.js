48ad0c20e7e52053bb97637892b23f0499a2751e
prmkvlad <prmk.vlad@gmail.com>
2023-09-02 15:42:58 +0300

    Fix problem with burger on mobile devices

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,33 @@
-const menu = document.querySelector('.menu__body')
-const menuBtn = document.querySelector('.menu__icon')
+/*=================burger=================*/
+
+const menu = document.querySelector('.menu__body');
+const menuBtn = document.querySelector('.menu__icon');
 
 const body = document.body;
 
+function topFunction() {
+	body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 if (menu && menuBtn) {
 	menuBtn.addEventListener('click', () => {
-		body.scrollTo({ top: 0 })
-		menu.classList.toggle('active')
-		menuBtn.classList.toggle('active')
-		body.classList.toggle('lock')
+		topFunction();
+		menu.classList.toggle('active');
+		menuBtn.classList.toggle('active');
+		body.classList.toggle('lock');
 	})
 
 	menu.querySelectorAll('.menu__link').forEach(link => {
 		link.addEventListener('click', () => {
-			menu.classList.remove('active')
-			menuBtn.classList.remove('active')
-			body.classList.remove('lock')
+			menu.classList.remove('active');
+			menuBtn.classList.remove('active');
+			body.classList.remove('lock');
 		})
 	})
 }
 
-/*========================================*/
+/*=================smooth scroll=================*/
 
 const anchors = document.querySelectorAll('a[href*="#"]');
 
